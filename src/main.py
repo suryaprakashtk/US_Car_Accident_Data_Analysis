4b8644e3e6ea238a28880c45186c17b6a6fd5d5d
Surya <stk222@ucsd.edu>
2024-11-08 18:41:16 -0800

    changed requirements file and main file

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-print("Welcome to Team 8 ECE143 Course Final Project")
+import pandas as pd
+
+path = './Dataset/US_Accidents_March23.csv'
+
+df = pd.read_csv(path, nrows=10) 
+column_names = df.columns
+print(column_names)
+stats = df.describe()
+print(stats)
+
+x = 5
